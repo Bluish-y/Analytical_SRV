6306f6fc8dcbda2b8fc551ef4077c7bcc708e914
Yashee Sinha <f20190652@goa.bits-pilani.ac.in>
2024-06-01 16:04:46 +0200

    Add files via upload

---
+++ compare.py
@@ -0,0 +1,99 @@
+import my_modules.analyticalsrv_func as asrv
+import my_modules.direct_srv as ds
+import numpy as np
+from matplotlib import pyplot as plt
+from sympy import sympify
+
+nsims = 50
+ISXs_direct = np.zeros(nsims)
+ISXis_direct = np.zeros(nsims)
+ISXs_analytical = np.zeros(nsims)
+ISXis_analytical = np.zeros(nsims)
+
+for i in range(0, nsims):
+    p = np.random.dirichlet([1,1,1])
+    q = np.random.dirichlet([1,1,1])
+
+    #test
+    # q,p = np.array([0.55373569, 0.23203089, 0.21423342]) ,  np.array([0.25712055, 0.53742108, 0.20545838])
+#     q,p = np.array([0.65, 0., 0.35]) ,  np.array([0.85, 0.15, 0])
+#     q,p = np.array([0.65, 0.001, 0.349]) ,  np.array([0.85, 0.149, 0.001])
+
+    ps = [p,q]
+
+    kxs = tuple(len(p) for p in ps)  # number of values for each input variable
+    ks = max(kxs)  # number of states for the SRV to be constructed
+    srv_ps_shape = kxs + (ks,)  # for convenience, used in multiple places below
+    on_violation = 'continue'
+    direct_srv_joint, totviol_joint = ds.compute_direct_srv_assuming_independence(ps, ks, method='joint', return_also_violation=True, 
+                                                                                on_violation=on_violation, verbose=1)
+
+    analytical_srv1 = asrv.analyticalsrv_test(q,p, do_it=0, search_it = 1, do_and_search=0, print_it=0)
+    analytical_srv2 = asrv.cubic_analyticalsrv_test(q,p, do_it=1, search_it = 1, do_and_search=1, print_it=0)
+
+
+    ISXs_direct[i] = ds.mutual_information_srv_all_inputs(direct_srv_joint, ps)
+    ISXis_direct[i] = np.sum([abs(ds.mutual_information_srv_given_single_input(xix, ps, direct_srv_joint)) for xix in range(len(ps))])
+
+    ISXs_analytical[i] = max(ds.mutual_information_srv_all_inputs(analytical_srv1, ps), ds.mutual_information_srv_all_inputs(analytical_srv2, ps))
+    ISXis_analytical[i] = max(np.sum([abs(ds.mutual_information_srv_given_single_input(xix, ps, analytical_srv2)) for xix in range(len(ps))]),
+                              np.sum([abs(ds.mutual_information_srv_given_single_input(xix, ps, analytical_srv2)) for xix in range(len(ps))]))
+
+
+print(f'I(S:X) = {ds.mutual_information_srv_all_inputs(direct_srv_joint, ps):.2f}. ' 
+      + f'H(X_i)={[f"{ds.entropy_input(xix, ps):.2f}" for xix in range(len(ps))]}'
+      + f'. I(S:X_i)={[f"{ds.mutual_information_srv_given_single_input(xix, ps, direct_srv_joint):.5f}" for xix in range(len(ps))]}')
+
+print(f'I(S:X) = {ds.mutual_information_srv_all_inputs(analytical_srv2, ps):.2f}. ' 
+      + f'H(X_i)={[f"{ds.entropy_input(xix, ps):.2f}" for xix in range(len(ps))]}'
+      + f'. I(S:X_i)={[f"{ds.mutual_information_srv_given_single_input(xix, ps, analytical_srv2):.5f}" for xix in range(len(ps))]}')
+
+
+# x = np.arange(nsims)  # Generate x values for the range of simulations
+data1 = [ISXs_analytical, ISXs_direct, np.maximum(ISXs_analytical, ISXs_direct)]
+data2 = [ISXis_analytical, ISXis_direct]
+
+plt.figure(figsize=(10, 8))  # Optional: Adjust the figure size
+
+# Create the first subplot
+plt.subplot(2, 1, 1)  # Create a subplot (2 rows, 1 column, plot 1)
+plt.boxplot(data1, labels=['Analytical Approach', 'Numerical Approach', 'max of both'], patch_artist=True)
+plt.ylabel(r"$I(S:X)$")
+plt.title("(a)")
+# plt.legend()
+
+# Create the second subplot
+plt.subplot(2, 1, 2)  # Create a subplot (2 rows, 1 column, plot 2)
+plt.boxplot(data2, labels=['Analytical Approach', 'Numerical Approach'])
+plt.ylabel(r"$\sum_{i=1}^{n} |I(S:X_i)|$")
+plt.title("(b)")
+
+plt.subplots_adjust(hspace=0.5)  # Adjust the space between plots
+plt.show()
+
+
+x = np.arange(nsims)  # Generate x values for the range of simulations
+width = 0.35  # Width of the bars
+space = 0.15  # Additional space between groups
+
+# plt.figure(figsize=(10, 8))  # Optional: Adjust the figure size
+
+# Create the first subplot
+plt.subplot(2, 1, 1)  # Create a subplot (2 rows, 1 column, plot 1)
+plt.bar(x * (1 + space) - width/2, ISXs_analytical, width, label='Analytical Approach', color='b')
+plt.bar(x * (1 + space) + width/2, ISXs_direct, width, label='Numerical Approach', color='r')
+plt.title(r"$I(S:X)$")
+plt.legend()
+plt.gca().set_xticklabels([])  # Remove x-axis labels
+
+# Create the second subplot
+plt.subplot(2, 1, 2)  # Create a subplot (2 rows, 1 column, plot 2)
+plt.bar(x * (1 + space) - width/2, ISXis_analytical, width, label='Analytical Approach', color='g')
+plt.bar(x * (1 + space) + width/2, ISXis_direct, width, label="Numerical Approach", color='m')
+plt.title(r"$\sum_{i=1}^{n} |I(S:X_i)|$")
+plt.xlabel("Different values of " + r'$\vec{p}, \vec{q}$')
+plt.legend()
+plt.gca().set_xticklabels([])  # Remove x-axis labels
+
+plt.subplots_adjust(hspace=0.5)  # Adjust the space between plots
+plt.show()
---
+++ main.py
@@ -0,0 +1,111 @@
+import numpy as np
+import warnings
+from my_modules.helpers import *
+import my_modules.variables as vb
+import my_modules.direct_srv as ds
+import my_modules.analyticalsrv_func as asrv
+np.set_printoptions(suppress=True)
+
+#best case
+ps = np.array([0.2791386,  0.28731193, 0.43354946])
+qs = np.array([0.39314196, 0.3746973,  0.23216074])
+
+#ahab's great whale
+# qs = np.array([0.81134934, 0.15412982, 0.03452084])
+# ps = np.array([0.14296979, 0.08690643, 0.77012377]) 
+
+#control
+# ps,qs = np.array([0.4, 0.32, 0.28]), np.array([0.5, 0.35, 0.15])
+
+# ps,qs = np.zeros(3), np.zeros(3)
+# for i in range(0,2):
+#     ps[i] = np.random.uniform(0.2,0.6-ps[(i-1)%3])
+#     qs[i] = np.random.uniform(0.2,0.6-qs[(i-1)%3])
+# ps[2],qs[2] = 1-ps[0]-ps[1], 1-qs[0]-qs[1]
+# ps = np.random.dirichlet([1,1,1])
+# qs = np.random.dirichlet([1,1,1])
+
+#### derived variables #####
+srv_states = 3
+p_states = ps.shape[0]
+q_states = qs.shape[0]
+#### ----------------- #####
+
+#### analytically calculated ####
+master_mat = np.concatenate((np.kron(np.eye(p_states), ps), np.kron(qs, np.eye(q_states))), axis = 0)
+pinv = np.linalg.pinv(master_mat)
+null_mat = np.eye(9) - np.dot(pinv, master_mat)
+pinv_nm = null_mat
+#### ---------------------- ####
+
+#### loop variables ####
+prev_srvs = []
+prev_sum = 0
+srvs = []
+# r_srvs, r_srv= vb.give_rick_srvs(ps,qs)
+# bs, target_marginal = vb.give_rick_bs(r_srv,ps,qs)
+# ws = vb.give_rick_ws(r_srvs, bs, pinv, pinv_nm)
+# print_info(r_srv, qs,ps)
+
+target_marginal = ps[::-1]
+bs = np.array([target*np.ones(p_states+q_states) for target in target_marginal])
+ws = [np.zeros(p_states*q_states)]*3
+i_srv = asrv.generate_simple_srv(ps,qs,bs,ws,pinv,null_mat,master_mat)
+print_info(i_srv, qs, ps)
+
+# ws = [1*np.array([0.5, 0, 0.2, 0, 0.5, 0, 0, 0, 1]), 1*np.array([0, 0, 0.7, 0.7, 0, 0, 0, 1, 0])]
+
+########################
+
+
+for i in range(0, srv_states-1):
+    #we find the solution assuming the solution['free_params'] = 0 and then solution['put_one_in'] = 1:
+    b,w = bs[i], ws[i]
+    solution = np.dot(pinv, b) + np.dot(null_mat, w)
+    solution[abs(solution)<1e-8] = 0
+    solution[abs(1-solution)<1e-8] = 1
+    
+
+    #The above step only works when the target_marginal is chosen wisely:
+    assert np.all(abs(np.dot(master_mat, solution)-bs[i])<1e-7)
+    
+    #grad calculation
+    grad_b, grad_w = grad_info(solution, ps,qs, target_marginal, w, pinv, null_mat)
+
+    step = 0.05
+    target_marginal += step*grad_b
+    b = target_marginal[i]*np.ones(p_states+q_states)
+    w += step*grad_w
+
+    solution = np.dot(pinv, b) + np.dot(null_mat, w)
+
+    prev_sum += solution
+    prev_srvs.append(solution)
+    srvs.append(solution.reshape(q_states,p_states))
+prev_srvs.append(1-prev_sum)
+
+for k in range(0, srv_states):
+    target_marginal[k] = np.dot(master_mat, prev_srvs[k])[0]
+
+#calculate the last element of the srvs##
+A, B, C = impurify(srvs[0], srvs[1], (1-prev_sum).reshape(3,3), do_it = 0)
+srvs.append(C)
+## ----------------------------------##
+
+#### finallyyyy ####
+srv = np.concatenate([matrix[..., np.newaxis] for matrix in srvs], axis=2)
+srv[abs(srv)< 1e-13] = 0
+##----------------#
+
+marginals = calculate_marginals(srv, ps, qs).reshape(6,3)
+print("\nFinal marginals:", marginals,'\n')
+any_mismatch = np.any(abs(marginals - marginals[0])>1e-2, axis=1)
+generate_uhoh(not np.any(any_mismatch))
+
+mutual_info, entropy = calculate_mutual_info(srv, ps,qs, srv_states)
+print("\nMutual info and entropy:", mutual_info, entropy, "\n")
+
+print("\nSrv:\n",srv, "\n ps, and qs, end_marginal:", repr(ps), ", ", repr(qs), ", ", repr(marginals[0]))
+print_info(srv, ps, qs)
+
+# np.save('srv.npy', srv)
